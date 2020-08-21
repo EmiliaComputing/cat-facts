c5cb8f3805303d33491480a8a17fedaf486a2281
Simon Pitcher <simon_pitcher@Simon-Pitchers-MacBook-Air.local>
2020-08-21 18:56:08 +0100

    first commit

---
+++ cat facts.py
@@ -0,0 +1,22 @@
+import urllib.request
+import json
+import ssl
+import random
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+url = 'https://cat-fact.herokuapp.com/facts'
+response = urllib.request.urlopen(url)
+json_data = response.read()
+cat_data = json.loads(json_data)
+all_facts = cat_data['all']
+#for fact in all_facts[1:10]:
+##    text = fact['text']
+##    print(text)
+##    print()
+
+while True:
+    prompt = input('press enter to get a random cat fact')
+    random_fact = random.choice(all_facts)
+    print(random_fact['text'])
+    print()
